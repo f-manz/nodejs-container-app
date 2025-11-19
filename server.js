d703204a13c700ebe4f59a3c6cfdf6dba5a32eae
fatima <manzurf@sheridancollege.ca>
2025-11-18 21:25:37 -0500

    Trigger GitHub Actions workflow

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Dockerized Node.js App!');
+  res.send('Hello, Dockerized Node.js App! Updated!');
 });
 
 app.listen(PORT, () => {
   console.log(`App running on http://localhost:${PORT}`);
 });
- 
+
