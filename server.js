e361d4cb3b4c9e94154c5ae60d4dc96c7f1bb016
fatima <manzurf@sheridancollege.ca>
2025-11-18 22:08:10 -0500

    Trigger workflow again after fixing Docker token

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Dockerized Node.js App! Updated!');
+  res.send('Hello, Dockerized Node.js App! Updated 3!');
 });
 
 app.listen(PORT, () => {
